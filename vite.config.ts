76fbac5b12ba43aee16a2d3ef564cae02ab5be7f
giorgileladze <giorgileladze78@gmail.com>
2024-02-29 20:39:22 +0400

    install vite-plugin-checker to handle TSchecks, change default port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  }
 })
