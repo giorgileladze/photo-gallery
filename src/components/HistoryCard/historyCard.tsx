ca497147d098bbb7b96f2ebc4fd0469383a26235
giorgileladze <giorgileladze78@gmail.com>
2024-03-03 15:34:59 +0400

    implement history card component

---
+++ src/components/HistoryCard/historyCard.tsx
@@ -0,0 +1,32 @@
+import './style.css'
+
+interface PorpTypes {
+    keys: string[]
+    active: string
+    onClick: (key: string) => void
+}
+
+const HistoryCard: React.FC<PorpTypes> = ({keys, active, onClick}) => {
+    const handleClick = (key: string) => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+        onClick(key)
+    }
+
+  return (
+    <div className='history-card'>
+        {keys.map(el => {
+            return (
+                <span className={`${active === el ? 'active' : ''}`} onClick={() => handleClick(el)}>
+                    {el}
+                </span>
+            )
+        })
+        }
+    </div>
+  )
+}
+
+export default HistoryCard
