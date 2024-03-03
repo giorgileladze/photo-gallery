27216e356932721adcb0350683161cf01b463b86
giorgileladze <giorgileladze78@gmail.com>
2024-03-03 15:34:33 +0400

    implement pages

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import PhotosList from "../components/PhotosList";
+import useInfinitySearch from "../hooks/useInfinitySearch";
+import usePhotosSearch from "../hooks/usePhotosSearch";
+import useHistory from "../hooks/useHistory";
+import HistoryCard from "../components/HistoryCard";
 
 const History: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [active, setActive] = useState<string>('')
+  const history = useHistory();
+  const { 
+    photos,
+    loading,
+    hasMore,
+  } = usePhotosSearch(active, page, false);
+
+  const increment = () => {
+    setPage(prev => prev + 1);
+  }
+  
+  const { reference } = useInfinitySearch({loading, hasMore, callback: increment});
+
+  const handleClick = (key: string) => {
+    setActive(key);
+  }
+
+  useEffect(() => {
+    console.log(history)
+  }, [page])
+
   return (
-    <div>history</div>
+    <div>
+      <HistoryCard keys={history} onClick={handleClick} active={active}/>
+      <PhotosList data={photos} reference={reference}/>
+    </div>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useState } from "react"
 import PhotosList from "../components/PhotosList"
 import usePhotosSearch from "../hooks/usePhotosSearch";
 import Search from "../components/Search";
+import useInfinitySearch from "../hooks/useInfinitySearch";
 
 const Home: React.FC = () => {
  const [page, setPage] = useState(1);
@@ -12,18 +13,11 @@ const Home: React.FC = () => {
     hasMore,
   } = usePhotosSearch(query, page);
 
-  const observer = useRef<IntersectionObserver | null>(null)
-  const lastBookElementRef = useCallback((node: HTMLImageElement) => {
-    if (loading) return;
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPage(prevPageNumber => prevPageNumber + 1)
-      }
-    })
-    if (node) observer.current.observe(node)
-  }, [loading, hasMore])
-
+  const increment = () => {
+    setPage(prev => prev + 1);
+  }
+  
+  const { reference } = useInfinitySearch({loading, hasMore, callback: increment});
 
   const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const val = e.target.value;
@@ -35,20 +29,10 @@ const Home: React.FC = () => {
     setPage(1);
   }
 
-  useEffect(() => {
-    const set = new Set();
-
-    photos.forEach(el => set.add(el.id));
-
-    if(photos.length > set.size) {
-      console.log(photos)
-    }
-  }, [photos])
-
   return (
     <div>
       <Search handleQueryChange={handleQueryChange}/>
-      <PhotosList data={photos} reference={lastBookElementRef}/>
+      <PhotosList data={photos} reference={reference}/>
     </div>
   )
 }
