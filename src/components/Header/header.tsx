bf716d1e7fb5f9f979d9e46239a8bdcfa7a5e136
giorgileladze <giorgileladze78@gmail.com>
2024-03-03 14:15:40 +0400

    implement basic header component

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,17 +2,13 @@ import { NavLink } from 'react-router-dom'
 import './style.css'
 import logo from '../../assets/logo.svg'
 
-function Header() {
-
+const Header: React.FC = () => {
     return (
         <header>
             <div className="logo">
                 <NavLink to="/">
                     <img src={logo} alt='Logo'/>
                 </NavLink>
-            </div>
-            <div className='search'>
-                <input type='text' placeholder='Search' />
             </div>
             <div className='menu'>
                 <nav>
